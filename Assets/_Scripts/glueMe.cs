b3fb35151a32fe9e3787a00b715a7a4bc0b6e178
006795 <martin.hering@stustanet.de>
2016-12-21 14:15:42 +0100

    glueing

---
+++ Assets/_Scripts/glueMe.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class glueMe : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wood")
+        {
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                if (this.gameObject.transform.parent == null)
+                {
+                    //FixedJoint joint = this.gameObject.AddComponent<FixedJoint>();
+                    this.gameObject.transform.rotation = collision.gameObject.transform.rotation;
+                    Rigidbody rigi = this.gameObject.GetComponent<Rigidbody>();
+                    //   rigi.isKinematic = true;
+                    this.transform.SetParent(collision.gameObject.transform);
+                }
+                else
+                {
+                    collision.gameObject.transform.SetParent(this.gameObject.transform.parent);
+                }
+
+                // joint.connectedBody = collision.gameObject.GetComponent<Rigidbody>();
+
+                return;
+            }
+        }
+    }
+
+}
