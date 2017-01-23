c63024682b0bf281420515461fd61acaaec2b475
martin <martin.hering@campus.lmu.de>
2017-01-23 11:53:34 +0100

    add ponitcounter

--- Assets/_Scripts/CuttingGameController.cs
+++ Assets/_Scripts/CuttingGameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class CuttingGameController : GameController {
 
@@ -11,12 +12,15 @@ public class CuttingGameController : GameController {
     public int score = 0;
     public int miss = 0;
 
+    public GameObject scoreCounter;
+    private Text txtScore;
+
     public bool playing = true;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        txtScore = scoreCounter.GetComponent<Text>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -26,6 +30,7 @@ public class CuttingGameController : GameController {
 
     public void addScore(int newScore) {
         score += newScore;
+        txtScore.text = score.ToString();
     }
 
     public void addMiss(int newMiss)
