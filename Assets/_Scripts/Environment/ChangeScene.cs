77d45a8ed678eaab513fd0f8aebce747e7758e64
martin <martin.hering@campus.lmu.de>
2017-01-24 11:18:12 +0100

    add screenswitcher

---
+++ Assets/_Scripts/Environment/ChangeScene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+public class ChangeScene : Interactable
+{
+    // Use this for initialization
+    AudioSource audioClip;
+    // Use this for initialization
+    void Start()
+    {
+        audioClip = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+    public override void interact(GameObject interactionGO)
+    {
+        audioClip.Play();
+        if(SceneManager.GetActiveScene().name == "MainScene")
+             SceneManager.LoadScene("Laser");
+        if (SceneManager.GetActiveScene().name == "Laser")
+            SceneManager.LoadScene("HammerFall");
+        if (SceneManager.GetActiveScene().name == "HammerFall")
+            SceneManager.LoadScene("MainScene");
+    }
+}
