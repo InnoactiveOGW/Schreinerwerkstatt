e322502d0ce03eb9eaa40cc805486da5826048c7
martin <martin.hering@campus.lmu.de>
2017-01-24 11:28:25 +0100

    change to vrmode

---
+++ Assets/_Scripts/Environment/ResetAll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+public class ResetAll : Interactable
+{
+    // Use this for initialization
+    AudioSource audioClip;
+    // Use this for initialization
+    void Start()
+    {
+        audioClip = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+    public override void interact(GameObject interactionGO)
+    {
+        audioClip.Play();
+       SceneManager.LoadScene("MainScene");
+      
+    }
+}
